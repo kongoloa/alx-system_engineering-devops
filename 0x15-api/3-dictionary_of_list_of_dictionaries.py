0ab9ba820147260c9baab1cd9374f43d69bfdab6
kongoloa <aristoteshako@gmail.com>
2022-04-11 15:19:21 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-import json
-import requests
-from sys import argv
-""" access a URL with employee ID to return information """
+""" accessing a url with employee ID to return information """
+from json import dump
+from requests import get
 
-if __name__ == "__main__":
-    users = requests.get("https://jsonplaceholder.typicode.com/users",
-                         verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
-                        verify=False).json()
-    user_dict = {}
-    usernamedict = {}
-    for user in users:
-        ID = user.get("id")
-        user_dict[ID] = []
-        usernamedict[ID] = user.get('username')
-    for task in todo:
-        task_dict = {}
-        ID = task.get("userId")
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict['username'] = usernamedict.get(ID)
-        user_dict.get(ID).append(task_dict)
-    with open("todo_all_employees.json", 'w') as jsfile:
-        json.dump(user_dict, jsfile)
+USERS = 'https://jsonplaceholder.typicode.com/users'
+TODOS = 'https://jsonplaceholder.typicode.com/todos'
+
+if __name__ == '__main__':
+    with open('todo_all_employees.json', 'w') as ostream:
+        dump({
+            str(user['id']): [{
+                "username": user['username'],
+                "task": task['title'],
+                "completed": task['completed'],
+            } for task in get(TODOS, params={'userId': user['id']}).json()]
+            for user in get(USERS).json()
+        }, ostream)
