5413b952529ca5e02b395267e6efff0922651459
kongoloa <aristoteshako@gmail.com>
2022-04-11 15:10:07 -0700

    to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-import csv
-import requests
+""" accessing a url with employee ID to return information """
+from argparse import ArgumentParser
+from os import path
+from csv import QUOTE_ALL, writer
+from requests import get
 from sys import argv
-""" access a url with employee ID to return information """
 
-if __name__ == "__main__":
-    """ function to export the data into csv file """
-    ID = int(argv[1])
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(ID)).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(ID)).json()
-    with open("{}.csv".format(ID), 'w') as csvf:
-        filler = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_ALL)
-        for task in todo:
-            filler.writerow([ID, user.get('username'),
-                            task.get('completed'), task.get('title')])
+USERS = 'https://jsonplaceholder.typicode.com/users'
+TODOS = 'https://jsonplaceholder.typicode.com/todos'
+
+if __name__ == '__main__':
+    parser = ArgumentParser(prog=path.basename(argv[0]))
+    parser.add_argument('id', type=int, help='employee ID')
+    args = parser.parse_args()
+    user = get('/'.join([USERS, str(args.id)])).json()
+    with open('.'.join([str(args.id), 'csv']), 'w', newline='') as ostream:
+        writer(ostream, quoting=QUOTE_ALL).writerows(
+            [str(args.id), user['username'], task['completed'], task['title']]
+            for task in get(TODOS, params={'userId': args.id}).json())
