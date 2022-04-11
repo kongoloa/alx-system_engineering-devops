a30f7a34d15a44276f9ff76c7eddf163990276e5
kongoloa <aristoteshako@gmail.com>
2022-04-11 13:45:05 -0700

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import csv
+import requests
+from sys import argv
+""" access a url with employee ID to return information """
+
+if __name__ == "__main__":
+    """ function to export the data into csv file """
+    ID = int(argv[1])
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(ID)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(ID)).json()
+    with open("{}.csv".format(ID), 'w') as csvf:
+        filler = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_ALL)
+        for task in todo:
+            filler.writerow([ID, user.get('username'),
+                            task.get('completed'), task.get('title')])
