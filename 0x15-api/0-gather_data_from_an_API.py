9fd00961fd3b5de1d968cb84eccc03d00b15f955
kongoloa <aristoteshako@gmail.com>
2022-04-11 14:34:01 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-""" accessing a url with employee ID to return information """
-from argparse import ArgumentParser
-from os import path
-from requests import get
-from sys import argv
-
-API = 'https://jsonplaceholder.typicode.com'
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
 
 if __name__ == '__main__':
-    parser = ArgumentParser(prog=path.basename(argv[0]))
-    parser.add_argument('id', type=int, help='employee ID')
-    args = parser.parse_args()
-    user = get('/'.join([API, 'users', str(args.id)])).json()
-    todo = get('/'.join([API, 'todos']), params={'userId': args.id}).json()
-    completed = [task for task in todo if task['completed'] is True]
-    print('Employee {} is done with tasks({}/{}):'.format(
-        user['name'], len(completed), len(todo)))
-    print('\n'.join('\t {}'.format(task['title']) for task in completed))
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
