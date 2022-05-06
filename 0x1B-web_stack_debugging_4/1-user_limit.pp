d296308f90132d10ff1644ee8dcdf9e2539a2688
kongoloa <aristoteshako@gmail.com>
2022-05-05 17:00:25 -0700

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
-# Increase limit of open files per user
-exec {'hard-limit':
-    command  => 'sed -i "/holberton hard/s/5/10000" /etc/security/limits.conf',
-    provider => shell,
-}
-
-exec {'soft-limit':
-    command  => 'sed -i "/holberton hard/s/4/10000" /etc/security/limits.conf',
-    provider => shell,
+# Web Stack debugging increase limit of open files per user
+exec { 'change-os-configuration-for-holberton-user':
+  environment => ['DIR=/etc/security/limits.conf',
+                  'OLD=hard nofile 5',
+                  'NEW=hard nofile 50000',
+                  'OLD2=soft nofile 4',
+                  'NEW2=soft nofile 40000'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
 }
